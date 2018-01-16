2abbae39c3d0621745a0293bf22a57db421698d5
Inigo <Inigo@192.168.0.102>
2018-01-16 17:01:24 +0100

    FIXED RINGGZ CLASS (kinda)

--- Ringgz/src/Ringgz.java
+++ Ringgz/src/Ringgz.java
@@ -7,8 +7,9 @@
 public class Ringgz {
     public static void main(String[] args) {
     	Player[] player = new Player[4]; //WE HAVE TO ADD AN OPTION TO PLAY WITH 2, 3 OR 4 PLAYERS
+    	Color c = Color.RED; //INITIALIZE THIS VALUE AS RED AND THEN ROTATE FOR NEXT PLAYERS
     	for (int i = 0; i < args.length; i++) {
-    		Color color = Color.RED; //INITIALIZE THIS VALUE AS RED AND THEN ROTATE FOR NEXT PLAYERS
+    		Color color = c; 
     		if (args[i].equals("-N")) {
     			player[i] = new ComputerPlayer(color, new NaiveStrategy());
     		} else {
@@ -20,7 +21,7 @@ public class Ringgz {
     		}
     		color.next(); //THIS IS WHAT ROTATES THE COLOR
     	}
-        Game g = new Game(player[0], player[1],player[2],player[3]);
+        Game g = new Game(player[0], player[1],player[2],player[3]); //MAKE THIS NOT HARDCODED FOR 4 PLAYERS;
         g.start();
     }
 }
