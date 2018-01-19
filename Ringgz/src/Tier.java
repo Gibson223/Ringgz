52bda06c4fc6c93572ad0c576282a543e0ecad43
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-01-19 13:52:49 +0100

    toTier

--- Ringgz/src/Tier.java
+++ Ringgz/src/Tier.java
@@ -7,4 +7,20 @@ public enum Tier{
 			return false;
 		}
 	}
+	public Tier toTier(int i) {
+		if (i == 1) {
+			return Tier.SMALL;
+		} else if(i == 2) {
+			return Tier.MEDIUM;
+		} else if (i == 3) {
+			return Tier.LARGE;
+		} else if (i == 4) {
+			return Tier.LARGEST;
+		} else if (i == 5) {
+			return Tier.BASE;
+		} else {
+			return null;
+		}
+		
+	}
 }
