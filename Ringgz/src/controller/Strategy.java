2bdd2265e02f84599a3e167627a34aec32edc1be
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-01-24 11:19:15 +0100

    updated methods

--- Ringgz/src/controller/Strategy.java
+++ Ringgz/src/controller/Strategy.java
@@ -2,10 +2,13 @@ package controller;
 import model.Board;
 import model.Color;
 import model.Ring;
+import model.*;
 
 public interface Strategy {
 	
 	public String getName();
 	public int determineMove(Board b, Ring r);
+	public Field determineField(Color c);// returns adjacent fields minus full fields
+	public Tier determineTier(Field f);
 
 }
