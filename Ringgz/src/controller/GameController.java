5455807ac1cd804b19141c686e417f2eb147c282
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-01-24 20:53:19 +0100

    i am going to fuck some shit up...

--- Ringgz/src/controller/GameController.java
+++ Ringgz/src/controller/GameController.java
@@ -8,22 +8,34 @@ public class GameController implements Runnable {
 	public Player[] players;
 	public Board board;
 	public TUI tui;
-	public RingList ringlist;
+	public RingList ringlist = new RingList();
 	// we get the name from the packets
 	//TODO ask question how to add observers to all fields of the board
 	public GameController( Player s0, Player s1, Player s2, Player s3) {
-		board = new Board();
-		tui = new TUI(this.board);
-		ringlist = new RingList();
+//		ringlist = new RingList();
         players = new Player[3];
-        players[0] = new HumanPlayer("Gibson", Color.BLUE, ringlist);
+        players[0] = s0;
         players[1] = s1;
         players[2] = s2;
         players[3] = s3;
+        this.run();
+	}
+	public GameController(HumanPlayer humanPlayer, HumanPlayer humanPlayer2) {
+		
 	}
 	@Override
 	public void run() {
+		for  (Field field: board.fields) {
+			field.addObserver(tui);
+		}
+		board = new Board();
+		tui = new TUI(this.board);
         tui.start();
+	}
+	public static void main(String[] args) {
+		GameController gc = new GameController(
+				new HumanPlayer("Gibson", Color.BLUE, Color.YELLOW, gc.ringlist),
+				new HumanPlayer("Random", Color.GREEN, Color.RED, gc.ringlist));
 	}
 	
 }
--- Ringgz/src/view/TUI.java
+++ Ringgz/src/view/TUI.java
@@ -1,6 +1,7 @@
 package view;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
 import controller.RinggzException;
 import model.Board;
@@ -38,13 +39,21 @@ public class TUI implements Observer {
 			b.setRing(8, ring3);
 		} catch (RinggzException e) {
 		}
-		tui.view();
+//		tui.view();
 	}
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		if (arg0 instanceof Field && arg1 instanceof String && ("ring placed".equals(((String)arg1)))) {
 			this.view();
 		}
+	}
+	public void start() {
+		this.view();
+//		boolean exit = false;
+//		while(!exit) {
+//			Scanner in = new Scanner(System.in);
+//			
+//		}
 	}
 
 
