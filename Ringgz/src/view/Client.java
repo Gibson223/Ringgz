9444e712da1e8744c4b7123fdd31cd19190c82d2
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-01-25 16:25:17 +0100

    immeadetly starts local game

--- Ringgz/src/view/Client.java
+++ Ringgz/src/view/Client.java
@@ -10,27 +10,29 @@ import model.*;
 import view.*;
 
 public class Client {
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
+//		localGame();
+//	}
+	public static void localGame() {
 		Player Gibson = new HumanPlayer("Gibson");
 		Player Random = new HumanPlayer("Random");
 		GameController gc = new GameController(Gibson, Random);
 		Thread game = new Thread(gc);
 		game.start();
 	}
-
-//	public static void main(String[] args) {
-//		System.out.println("please type in a ");
-//		Scanner in = new Scanner(System.in);
-//		String name = in.nextLine().split(" ")[0];
-//        try {
-//			InetAddress addr = InetAddress.getByName(in.nextLine().split(" ")[1]);
-//		} catch (UnknownHostException e) {
-//			System.out.println("wrong adress");
-//			e.printStackTrace();
-//		}
-//        int port = Integer.parseInt(in.nextLine().split(" ")[2]);
-//		;
-//		Socket client = new Socket();
-//	}
+	public static void main(String[] args) {
+		System.out.println("please type in a name, inetaddress and portnumber.....\n(separated by a space)");
+		Scanner in = new Scanner(System.in);
+		String name = in.nextLine().split(" ")[0];
+        try {
+			InetAddress addr = InetAddress.getByName(in.nextLine().split(" ")[1]);
+		} catch (UnknownHostException e) {
+			System.out.println("wrong adress");
+			e.printStackTrace();
+		}
+        int port = Integer.parseInt(in.nextLine().split(" ")[2]);
+		;
+		Socket client = new Socket();
+	}
 
 }
