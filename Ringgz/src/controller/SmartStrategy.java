4aeab1d63ea2e2588deb12ccecc3ad492b9acf2d
Inigo <Inigo@169.254.209.217>
2018-01-26 15:23:26 +0100

    made smartstrategy more compact and tidy but still get the same problem

--- Ringgz/src/controller/SmartStrategy.java
+++ Ringgz/src/controller/SmartStrategy.java
@@ -7,55 +7,48 @@ import java.util.List;
 import model.Board;
 import model.Color;
 import model.Field;
+import model.Ring;
 import model.Tier;
 
 public class SmartStrategy implements Strategy{
 
-	@Override
 	public String getName() {
 		return "Smart";
 	}
 	
-	@Override
 	public Field determineField(Board board, Player p) {
 		if (board.firstMove) {
 			try {
 				p.potentialFields.addAll(Arrays.asList(board.getField(7), board.getField(8), board.getField(9), board.getField(12), board.getField(13), board.getField(14), board.getField(17), board.getField(18), board.getField(19)));
-				boolean fieldFound = false;
-				while (!fieldFound) {
-					Collections.shuffle(p.potentialFields);
-					Field smartField = p.potentialFields.get(0);
-					int frec = Collections.frequency(smartField.getFieldColors(),p.getPrimaryColor());
-					if (frec > 2) {
-						determineField(board,p);
-					} else {
-						fieldFound = true;
-						return smartField;
-					}
-				}
 			} catch (RinggzException e) {
 				e.printStackTrace();
 			}
-		} else {
-			boolean fieldFound = false;
-			while (!fieldFound) {
-				Collections.shuffle(p.potentialFields);
-				Field smartField = p.potentialFields.get(0);
-				int frec = Collections.frequency(smartField.getFieldColors(),p.getPrimaryColor());
-				if (frec > 2) {
-					determineField(board,p);
-				} else {
-					fieldFound = true;
-					return smartField;
-				}
+		}
+		boolean fieldFound = false;
+		while (!fieldFound) {
+			Collections.shuffle(p.potentialFields);
+			Field smartField = p.potentialFields.get(0);
+			int frec = Collections.frequency(smartField.getFieldColors(),p.getPrimaryColor());
+			if (frec > 2) {
+				determineField(board,p);
+			} else {
+				fieldFound = true;
+				return smartField;
 			}
 		}
 	}
 
 	@Override
 	public Tier determineTier(Field f) {
-		// TODO Auto-generated method stub
-		return null;
+		List <Tier> availableTiers = new ArrayList<>();
+		availableTiers.removeAll(availableTiers);
+		for (Ring ring : f.getFieldState()) {
+			if (ring.getColor() == Color.INIT) {
+				availableTiers.add(ring.getTier());
+			}
+		}
+		Collections.shuffle(availableTiers);
+		return availableTiers.get(0);
 	}
 }
 
