575688a5e3c691c97ee4bc400c573540501e5ef8
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-01-29 16:41:05 +0100

    ringlistest

--- Ringgz/src/tests/RingListTest.java
+++ Ringgz/src/tests/RingListTest.java
@@ -4,27 +4,26 @@ import model.Ring;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import model.RingList;
 
-class RingListTest {
-	private RingList a;
-	@Before
-	void setUp() throws Exception {
-		RingList a = new RingList();
-//		a.availableRings.stream().forEach(b -> System.out.println(" " + b.getColor() + b.getTier(), rings.);
-		
-	}
-
+public class RingListTest {
+	public RingList a = new RingList();
+	public RingList b;
 	@Test
-	void test() {
-		assertTrue(a.availableRings.size() == 60);
+	public void test() {
 		assertTrue(a.availableRings.stream().filter(a -> a.getColor() ==Color.BLUE).count() == 15);
 		assertTrue(a.availableRings.stream().filter(a -> a.getColor() ==Color.YELLOW).count() == 15);
 		assertTrue(a.availableRings.stream().filter(a -> a.getColor() ==Color.RED).count() == 15);
 		assertTrue(a.availableRings.stream().filter(a -> a.getColor() ==Color.GREEN).count() == 15);
+		assertTrue(a.availableRings.size() == 60);
+		List<Ring> listshortened = a.availableRings.stream().filter(a -> a.getColor() ==Color.GREEN).collect(Collectors.toList());
+		b = new RingList(listshortened);
+		assertTrue(b.availableRings.size() == 15);
 	}
 }
