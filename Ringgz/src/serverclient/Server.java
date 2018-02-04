1aedea2b9f32331c5b2379c16595ffecfa82d085
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-02-04 20:57:55 +0100

    sserver checkstyle

--- Ringgz/src/serverclient/Server.java
+++ Ringgz/src/serverclient/Server.java
@@ -11,99 +11,98 @@ import java.util.List;
 import controller.GameController;
 
 public class Server implements Runnable {
-	public static int PORT = 1150;
-	private ServerSocket server;
-	private List<GameController> games;
-	private List<ClientHandler> clients;
-	private BufferedReader dis;
-	private BufferedWriter dos;
-	private boolean online;
-	public final int WAIT = 10000;
+    public static int PORT = 1150;
+    private ServerSocket server;
+    private List<GameController> games;
+    private List<ClientHandler> clients;
+    private BufferedReader dis;
+    private BufferedWriter dos;
+    private boolean online;
+    public final int WAIT = 10000;
 
-	public Server() throws IOException {
-		this.server = new ServerSocket(PORT);
-		this.games = new LinkedList<GameController>();
-		this.clients = new LinkedList<>();
-		online = true;
-	}
+    public Server() throws IOException {
+        this.server = new ServerSocket(PORT);
+        this.games = new LinkedList<GameController>();
+        this.clients = new LinkedList<>();
+        online = true;
+    }
 
-	// Starts the Server 
-	public static void main(String[] args) {
-		try {
-			Server s = new Server();
-			Thread sthread = new Thread(s);
-			sthread.start();
-			System.out.println("The server has been started.");
-			System.out.println("the port is " + s.PORT);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    // Starts the Server 
+    public static void main(String[] args) {
+        try {
+            Server s = new Server();
+            Thread sthread = new Thread(s);
+            sthread.start();
+            System.out.println("The server has been started.");
+            System.out.println("the port is " + s.PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void run() {
-		this.acceptClientSockets();
-		try {
-			this.shutDown();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void run() {
+        this.acceptClientSockets();
+        try {
+            this.shutDown();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void acceptClientSockets() {
-		while (online) {
-			try {
-				Socket clientSocket = server.accept();
-				ClientHandler clienthandler = new ClientHandler(this, clientSocket);
-				new Thread(clienthandler).start();
-				this.addHandler(clienthandler);
-				System.out.println("Assigned new thread for client" + clientSocket);
-			} catch (IOException e) {
-			}
-		}
-	}
+    public void acceptClientSockets() {
+        while (online) {
+            try {
+                Socket clientSocket = server.accept();
+                ClientHandler clienthandler = new ClientHandler(this, clientSocket);
+                new Thread(clienthandler).start();
+                this.addHandler(clienthandler);
+                System.out.println("Assigned new thread for client" + clientSocket);
+            } catch (IOException e) {
+            }
+        }
+    }
 
-	public GameController getGame(ClientHandler ch, int preferredplayers) {
-		if (this.games.size() != 0) {
-			for (GameController gc : this.games) {
-				if (gc.players.size() < gc.maxplayers && gc.addClient(ch)) {
-					gc.addPlayer(ch);
-					return gc;
-				}
-			}
-		} 
-		GameController newgame = new GameController(this, preferredplayers);
-		newgame.addClient(ch);
-		return newgame;
-	}
+    public GameController getGame(ClientHandler ch, int preferredplayers) {
+        if (this.games.size() != 0) {
+            for (GameController gc : this.games) {
+                if (gc.players.size() < gc.maxplayers && gc.addClient(ch)) {
+                    gc.addPlayer(ch);
+                    return gc;
+                }
+            }
+        }
+        GameController newgame = new GameController(this, preferredplayers);
+        newgame.addClient(ch);
+        return newgame;
+    }
 
-	public void serverPrint(String s) {
-		System.out.println(s);
-	}
+    public void serverPrint(String s) {
+        System.out.println(s);
+    }
 
-	public List<ClientHandler> getClients() {
-		return clients;
-	}
+    public List<ClientHandler> getClients() {
+        return clients;
+    }
 
-	private void addHandler(ClientHandler ch) {
-		this.clients.add(ch);
-	}
+    private void addHandler(ClientHandler ch) {
+        this.clients.add(ch);
+    }
 
-	private void removeHandler(ClientHandler ch) {
-		this.clients.remove(ch);
-	}
+    private void removeHandler(ClientHandler ch) {
+        this.clients.remove(ch);
+    }
 
-	// requires packet matches extension chatting
-	public void serverMessage(String message) throws IOException {
-		for (ClientHandler clienthandler : this.getClients()) {
-			clienthandler.sendmessage(message);
-		}
-	}
+    // requires packet matches extension chatting
+    public void serverMessage(String message) throws IOException {
+        for (ClientHandler clienthandler : this.getClients()) {
+            clienthandler.sendmessage(message);
+        }
+    }
 
-	private void shutDown() throws IOException {
-		this.dis.close();
-		this.dos.close();
-		this.server.close();
-	}
+    private void shutDown() throws IOException {
+        this.dis.close();
+        this.dos.close();
+        this.server.close();
+    }
 }
