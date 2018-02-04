2bbca9cab127c38fc1f1e5973242dfa69d63286d
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-02-04 21:13:24 +0100

    view checkstyle

--- Ringgz/src/view/Client.java
+++ Ringgz/src/view/Client.java
@@ -3,20 +3,22 @@ package view;
 import serverclient.*;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.Socket;
+
+import controller.LocalGameController;
 
 //Creates a client
 public class Client {
-	// Creates a client for the game
-	public static final int PORT = Server.PORT;
-	public static void main(String[] args) throws IOException {
-		TUI tui = new TUI();
-		Object[] input = tui.initizializing();
-		String username = (String) input[0];
-		int port = Integer.parseInt(tui.tuiInput("please give the port"));
-		InetAddress address = InetAddress.getByName((String) input[1]);
-		LocalGameController gc = new LocalGameController(tui, username, address, port);
-		new Thread(gc).run();
-		System.out.println("gc started");
-	}
+    // Creates a client for the game
+    public static final int PORT = Server.PORT;
+
+    public static void main(String[] args) throws IOException {
+        TUI tui = new TUI();
+        Object[] input = tui.initizializing();
+        String username = (String) input[0];
+        int port = Integer.parseInt(tui.tuiInput("please give the port"));
+        InetAddress address = InetAddress.getByName((String) input[1]);
+        LocalGameController gc = new LocalGameController(tui, username, address, port);
+        new Thread(gc).run();
+        System.out.println("gc started");
+    }
 }
--- Ringgz/src/view/TUI.java
+++ Ringgz/src/view/TUI.java
@@ -12,78 +12,78 @@ import model.Field;
 //tui for Ringgz
 public class TUI implements Observer, Runnable {
 
-	private Board board;
+    private Board board;
 
-	public static final String EXIT = "exit";
+    public static final String EXIT = "exit";
 
-	// Creates a tui with the given Game
-	public TUI() {
-		this.init();
-	}
+    // Creates a tui with the given Game
+    public TUI() {
+        this.init();
+    }
 
-	public void init() {
-		this.output("Insert username and IP separated by a space");
-	}
+    public void init() {
+        this.output("Insert username and IP separated by a space");
+    }
 
-	public Object[] initizializing() {
-		String username = null;
-		String address = null;
-		boolean correct = false;
-		while (!correct) {
-			Object input = this.tuiInput("-");
-			Object[] splitInput = ((String) input).split(" ");
-			correct = true;
-			if (splitInput.length != 2) {
-				correct = false;
-				this.output("Two arguments are needed. Please try again.");
-			}
-			username = (String) splitInput[0];
-			address = (String) splitInput[1];
-		}
-		return new Object[] {username, address };
-	}
+    public Object[] initizializing() {
+        String username = null;
+        String address = null;
+        boolean correct = false;
+        while (!correct) {
+            Object input = this.tuiInput("-");
+            Object[] splitInput = ((String) input).split(" ");
+            correct = true;
+            if (splitInput.length != 2) {
+                correct = false;
+                this.output("Two arguments are needed. Please try again.");
+            }
+            username = (String) splitInput[0];
+            address = (String) splitInput[1];
+        }
+        return new Object[] {username, address };
+    }
 
-	public String tuiInput(String string) {
-		String response = null;
-		System.out.println(string);
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			response = in.readLine();
-		} catch (IOException e) {
-		}
-		if (response == null) {
-			return tuiInput(string);
-		} else {
-			return response;
-		}
-	}
+    public String tuiInput(String string) {
+        String response = null;
+        System.out.println(string);
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            response = in.readLine();
+        } catch (IOException e) {
+        }
+        if (response == null) {
+            return tuiInput(string);
+        } else {
+            return response;
+        }
+    }
 
-	public void output(String message) {
-		System.out.println(message);
-	}
+    public void output(String message) {
+        System.out.println(message);
+    }
 
-	public void view() {
-		for (Field field : board.fields) {
-			if (((field.FieldNumber - 1) % 5) == 0) {
-				System.out.print("\n\n" + "|");
-			}
-			System.out.print(field.toString() + "|");
-		}
-	}
+    public void view() {
+        for (Field field : board.fields) {
+            if (((field.FieldNumber - 1) % 5) == 0) {
+                System.out.print("\n\n" + "|");
+            }
+            System.out.print(field.toString() + "|");
+        }
+    }
 
-	public void run() {
-		this.view();
-	}
+    public void run() {
+        this.view();
+    }
 
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		this.view();
-	}
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        this.view();
+    }
 
-	public void setBoard(Board board) {
-		this.board = board;
-		for (Field field : this.board.fields) {
-			field.addObserver(this);
-		}
-	}
+    public void setBoard(Board board) {
+        this.board = board;
+        for (Field field : this.board.fields) {
+            field.addObserver(this);
+        }
+    }
 }
