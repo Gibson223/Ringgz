5c4bd3a0b3219054895e4f4b3fd3e3af0c12e29a
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-02-04 20:48:19 +0100

    now going to work on checkstyle

--- Ringgz/src/controller/ServerPlayer.java
+++ Ringgz/src/controller/ServerPlayer.java
@@ -16,9 +16,6 @@ public class ServerPlayer extends Player {
 	@Override
 	public Move makeMove(Board board) throws RinggzException {
 		return this.clienthandler.getMove();
-		// TODO needs to depend on the input to the server of that specific client. i
-		// think we need to assign
-		// the inputstream of that client socket to this???
 	}
 	public boolean moveread;
 	private Move move;
