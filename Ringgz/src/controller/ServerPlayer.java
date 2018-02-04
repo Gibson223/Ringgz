c9c7fbf371711780cc1dded20c57942afb45b857
gibson vredeveld <gibso@LAPTOP-15AREPCS.home>
2018-02-04 20:51:16 +0100

    serverplayer checkstyle

--- Ringgz/src/controller/ServerPlayer.java
+++ Ringgz/src/controller/ServerPlayer.java
@@ -5,17 +5,31 @@ import serverclient.ClientHandler;
 
 public class ServerPlayer extends Player {
 	public ClientHandler clienthandler;
+
 	public ServerPlayer(String name, ClientHandler clienthandler) {
 		super(name);
 		this.clienthandler = clienthandler;
 		moveread = false;
 	}
+
 	public ServerPlayer(String name) {
 		super(name);
 	}
+
 	@Override
 	public Move makeMove(Board board) throws RinggzException {
-		return this.clienthandler.getMove();
+		if (this.clienthandler == null) {
+			return this.move;
+		} else {
+			return this.clienthandler.getMove();
+		}
 	}
-	public boolean moveread;	
+
+	public boolean moveread;
+	private Move move;
+
+	public void setMove(Move move) {
+		this.move = move;
+	}
+
 }
